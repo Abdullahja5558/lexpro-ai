e8e80f2f6230bdf231c6db922026b6b978d5dd38
Abdullah <abdullahjaved5558@gmail.com>
2026-04-24 23:04:10 +0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,7 +38,7 @@ export default function AboutSection({ isDark }: { isDark: boolean }) {
 
       // 1. Community: Growth logic preserved
       const communityGrowth = Math.floor(
-        (secondsElapsed / twoMinutesInSeconds) * 23.83,
+        (secondsElapsed / twoMinutesInSeconds) * 23.85,
       );
 
       
