1b3c0d09602036748629dc21a43982fbd2994ee9
Abdullah <abdullahjaved5558@gmail.com>
2026-04-27 18:26:43 +0500

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -46,7 +46,7 @@ export default function AboutSection({ isDark }: { isDark: boolean }) {
         (secondsElapsed / twoMinutesInSeconds) * 132,
       );
 
-      // 3. New "Jitter" Live Logic: Randomizes between 100 and 850 with sus
+      // 3. New "Jitter" Live Logic: Randomizes between 100 and 
       const generateRandomLive = () => {
         const chance = Math.random();
         if (chance > 0.9) return Math.floor(Math.random() * (850 - 600) + 600); // Sudden Spike
