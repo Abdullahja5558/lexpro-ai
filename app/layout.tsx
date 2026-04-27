1757b71ac2a0be6261a03c05b18fa5edc9809abb
Abdullah <abdullahjaved5558@gmail.com>
2026-04-27 18:29:20 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// 1. Viewport Settings: Mobile par zoom issues rokne ke liye
+// 1. Viewport Settings: Mobile par zoom issues rokn
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
