1363939f74d999a121124251ea74090e7e8a2228
Abdullah <abdullahjaved5558@gmail.com>
2026-04-27 18:29:32 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// 1. Viewport Settings: Mobile par zoom i
+// 1. Viewport Settings: Mobile p
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
